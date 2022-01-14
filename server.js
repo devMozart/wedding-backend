0208501d6fcd49fc68a09aac0a4171330da1cd65
Amadeus <amadeus.hein@cuviva.com>
2022-01-14 23:29:54 +0100

    Add guest endpoints

--- server.js
+++ server.js
@@ -1,24 +1,88 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/wedding';
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => console.log('Connected to DB'))
+  .catch((err) => console.log('Could not connect to DB', err));
+mongoose.Promise = Promise;
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+const Guest = mongoose.model('Guest', {
+  primaryGuest: { type: String, required: true },
+  secondaryGuest: { type: String },
+  information: { type: String },
+  date: { type: Date },
+});
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+const port = process.env.PORT || 8080;
+const app = express();
 
-// Start the server
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/guests', async (req, res) => {
+  try {
+    const guests = await Guest.find();
+    res.status(200).json({
+      response: {
+        guests,
+      },
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      response: {
+        error,
+      },
+      success: false,
+    });
+  }
+});
+
+app.post('/guests', async (req, res) => {
+  const { primaryGuest, secondaryGuest, information } = req.body;
+
+  if (!primaryGuest || primaryGuest.length === 0) {
+    res.status(400).json({
+      response: {
+        error: 'The primary guest cannot be empty',
+      },
+      success: false,
+    });
+  }
+
+  try {
+    const newGuest = new Guest({
+      primaryGuest,
+      secondaryGuest,
+      information,
+      date: new Date(),
+    });
+
+    await newGuest.save();
+
+    res.status(201).json({
+      response: {
+        guest: newGuest,
+      },
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      response: {
+        error,
+      },
+      success: false,
+    });
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
